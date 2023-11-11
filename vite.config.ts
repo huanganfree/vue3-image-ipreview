064e0319f47bd0b5becf34c57e2d25ef3cc850b8
huangan <huangan@meibaokeji.com>
2023-11-11 19:32:40 +0800

    更新发包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { join } from "path";
+import { join, resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +9,21 @@ export default defineConfig({
     alias: {
       '@': join(__dirname, "src"),
     }
-  }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/components/imagePreview/index.js'),
+      name: 'Vue3ImagePreview',
+      fileName: 'vue3-image-ipreview',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
